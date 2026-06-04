7612cb6c97a39f2cc3c15a2beeed3b54f81c52db
Rameez <rameez.haniff@gmail.com>
2026-06-04 12:43:55 +0200

    chore: initialize components and pages

---
+++ src/frontend/pantry-pilot-ui/src/app/pages/optimizer/optimizer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-optimizer',
+  imports: [],
+  templateUrl: './optimizer.component.html',
+  styleUrl: './optimizer.component.scss'
+})
+export class OptimizerComponent {
+
+}
